b362348679bf62504315e003c965380de70411af
edisonliu <edisonl2027@student.cis.edu.hk>
2024-08-28 09:27:53 +0800

    Homework

--- src/Dateable.java
+++ src/Dateable.java
@@ -1,6 +1,9 @@
 public class Dateable {
-
     public static void main(String[] args) {
-        
+        int age = 25; // replace with your age
+
+        int youngestAge = (age / 2) + 7;
+
+        System.out.println("The youngest person you can date is " + youngestAge + " years old.");
     }
 }
